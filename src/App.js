50643e28db4254db2e4f5fff5e4d69f82678f756
franco vercelli <vercellifranco@gmail.com>
2021-01-26 10:39:16 -0300

    initial commit added navbar component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+//components
+import MyNavbar from "./components/my-navbar/mynavbar.component";
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      
+        
+      <MyNavbar />
+        my portfolio
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/my-navbar/mynavbar.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from "react-bootstrap/Nav";
+import Logo from '../../assets/icons/logow.webp'
+import './mynavbar.style.css'
+
+const MyNavbar= () => {
+    return (
+        <>
+          <Navbar fixed ="top" collapseOnSelect expand="md"  variant="dark" className="animate-navbar nav-theme justify-content-between" >
+          <Navbar.Brand href="#home">
+              <img className="logo" src={Logo} alt='My Portfolio Logo' />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#p">About</Nav.Link>
+              <Nav.Link href="#pri">Skills</Nav.Link>
+              <Nav.Link href="#pricig">Experience</Nav.Link>
+              <Nav.Link href="#ping">Proyects</Nav.Link>
+              <Nav.Link href="#png">Contact</Nav.Link>
+     
+             </Nav>
+   
+  </Navbar.Collapse>
+</Navbar>
+        </>
+    )
+}
+
+export default MyNavbar
