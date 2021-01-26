f572c30a49de8559c83c811a6a66ca2b0c1b7bd4
franco vercelli <vercellifranco@gmail.com>
2021-01-26 16:17:13 -0300

    added my-carousal title-message scroll-down component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 
 //components
 import MyNavbar from "./components/my-navbar/mynavbar.component";
+import MyCarousal from "./components/my-carousal/my-carousal.component";
+import TitleMessage from './components/title-message/tittle-message.component'
 import './App.css';
 
 const App = () => {
@@ -10,7 +12,8 @@ const App = () => {
       
         
       <MyNavbar />
-        my portfolio
+      <MyCarousal />
+      <TitleMessage />
     </div>
   );
 }
